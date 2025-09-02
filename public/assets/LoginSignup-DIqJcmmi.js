2525576fa7d321ed9f4781abc0e09ae08c10a0ea
Valery Vishnevezky <valery020201@gmail.com>
2025-09-02 09:36:06 +0300

    Fix

---
+++ public/assets/LoginSignup-DIqJcmmi.js
@@ -0,0 +1,1 @@
+import{j as i,O as s}from"./index-B3Qt7UQK.js";function a(){const n=location.pathname.startsWith("/login/signup");return i.jsx("div",{className:"login-page",children:i.jsxs("div",{className:"login-form-container",children:[i.jsx("h1",{className:"title",children:n?"Signup":"Login"}),i.jsx(s,{})]})})}export{a as default};
